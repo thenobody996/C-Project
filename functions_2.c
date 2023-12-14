0260e121eb94460bae509b5f2829691e87a566f1
NSHYuki <3109209420@qq.com>
2023-12-14 11:15:16 +0800

    删除函数与保存函数

---
+++ functions_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "storesystem.h"
+
+void destory_list(GamesList **L)
+{
+    for (; *L; *L = (*L)->next)
+        free(*L);
+    CurrentCnt = 0;
+}
+
+void destory_list_and_file(GamesList **L)
+{
+    destory_list(L);
+    remove("gamesinfo.txt");
+}
+
+int SAVE_FILE(GamesList *L)
+{
+    if(!L)
+        return -1;
+
+    GamesList *p;
+    FILE* file = fopen(GOODS_FILE_NAME, "w");
+
+    int cnt = 0;
+    for(p = L->next; p; p = p->next)
+    {
+        fprintf(file, "%-25s\t%-35s\t%-8.2f\t-%-4.1f%\t%-8.2f\t%-8.2f\t", p->data.game_id, p->data.game_name, p->data.game_price_origin, p->data.game_discount, p->data.game_price_now, p->data.game_price_history);
+        cnt++;
+    }
+
+    fclose(file);
+    return cnt;
+}
