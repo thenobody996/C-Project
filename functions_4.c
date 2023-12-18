efd134a3526cba2d7bff75954fc29572503f849f
316974256 <153996505+316974256@users.noreply.github.com>
2023-12-18 12:33:56 +0800

    Update functions_4.c
    修改

--- functions_4.c
+++ functions_4.c
@@ -5,14 +5,9 @@
 #include <conio.h>
 #include "storesystem.h"
 
-void sort_Game_history(GamesList *L);
-void sort_Game_pricenow(GamesList *L);
-void sort_Game_discount(GamesList *L);
-
-
 void Sort_Game(GamesList **pL, int sortBy) {
     if (*pL == NULL || (*pL)->next == NULL) {
-        return;  // 链表为空或只有一个节点，无需排序
+        return;  
     }
 
     switch (sortBy) {
@@ -35,20 +30,18 @@ void Sort_Game(GamesList **pL, int sortBy) {
 // 以下是按照不同标准进行排序的辅助函数
 void sort_Game_history(GamesList *L) {
     if (L == NULL || L->next == NULL) {
-        return;  // 链表为空或只有一个节点，无需排序
+        return;  
     }
 
-    GamesList *sortedList = NULL;  // 排序后的链表
+    GamesList *sortedList = NULL;  
     GamesList *current = L;
 
     while (current != NULL) {
         GamesList *next = current->next;
         if (sortedList == NULL || sortedList->data.game_price_history > current->data.game_price_history) {
-            // 如果排序后的链表为空，或者当前节点的史低价格小于排序后链表的第一个节点
             current->next = sortedList;
             sortedList = current;
         } else {
-            // 在排序后的链表中找到合适的位置插入
             GamesList *temp = sortedList;
             while (temp->next != NULL && temp->next->data.game_price_history < current->data.game_price_history) {
                 temp = temp->next;
@@ -71,21 +64,19 @@ void sort_Game_history(GamesList *L) {
 void sort_Game_pricenow(GamesList *L) 
 {
     if (L == NULL || L->next == NULL) {
-        return;  // 链表为空或只有一个节点，无需排序
+        return;
     }
 
-    GamesList *sortedList = NULL;  // 排序后的链表
+    GamesList *sortedList = NULL; 
     GamesList *current = L;
 
     while (current != NULL) {
         GamesList *next = current->next;
         if (sortedList == NULL || sortedList->data.game_price_now > current->data.game_price_now) 
         {
-            // 如果排序后的链表为空，或者当前节点的史低价格小于排序后链表的第一个节点
             current->next = sortedList;
             sortedList = current;
         } else {
-        // 在排序后的链表中找到合适的位置插入
         GamesList *temp = sortedList;
         while (temp->next != NULL && temp->next->data.game_price_history < current->data.game_price_history) {
                 temp = temp->next;
@@ -106,20 +97,18 @@ void sort_Game_pricenow(GamesList *L)
 
 void sort_Game_discount(GamesList *L) {
     if (L == NULL || L->next == NULL) {
-        return;  // 链表为空或只有一个节点，无需排序
+        return;  
     }
 
-    GamesList *sortedList = NULL;  // 排序后的链表
+    GamesList *sortedList = NULL;  
     GamesList *current = L;
 
     while (current != NULL) {
         GamesList *next = current->next;
         if (sortedList == NULL || sortedList->data.game_discount > current->data.game_discount) {
-            // 如果排序后的链表为空，或者当前节点的史低价格小于排序后链表的第一个节点
             current->next = sortedList;
             sortedList = current;
         } else {
-            // 在排序后的链表中找到合适的位置插入
             GamesList *temp = sortedList;
             while (temp->next != NULL && temp->next->data.game_price_history < current->data.game_price_history) {
                 temp = temp->next;
